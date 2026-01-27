00a82d248fe29c559ad975f266a524b1b9fdbf34
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2026-01-27 12:16:13 +0100

    feat(presentatin-8mins): aggiungere presentazione durata 8 minuti
    Aggiunta presentazione ridotta per restare sotto gli 8 minuti

---
+++ create_presentation_8mins.py
@@ -0,0 +1,4 @@
+from start_venv import open_venv_shell
+  
+if __name__ == "__main__":
+    open_venv_shell("jupyter nbconvert notebooks/04_presentation_8mins.ipynb --to slides --SlidesExporter.reveal_number='c/t' --SlidesExporter.reveal_scroll=True --output-dir presentazione --output presentation_8mins & exit")
