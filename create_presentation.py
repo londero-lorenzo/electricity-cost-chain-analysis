c9746f2eb780b8e57221786e2b5b07a217a32785
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2026-01-28 11:51:00 +0100

    feat(presentation): aggiungere conversione per github pages
    Aggiunto script per converitre la presentazione prodotta da nbconvert in
    pagina index per github-pages

--- create_presentation.py
+++ create_presentation.py
@@ -1,4 +1,25 @@
 from start_venv import open_venv_shell
+import os
+
+JUPYTER_TARGET = "notebooks/04_presentation.ipynb"
+
+OUTPUT_FOLDER = "presentazione"
+OUTPUT_NAME = "index"
+
+
+
+NBCONVERT_OUTPUT_SUFFIX = "slides.html"
+
   
 if __name__ == "__main__":
-    open_venv_shell("jupyter nbconvert notebooks/04_presentation.ipynb --to slides --SlidesExporter.reveal_number='c/t' --SlidesExporter.reveal_scroll=True --output-dir presentazione --output presentation & exit")
+    open_venv_shell(f"jupyter nbconvert {JUPYTER_TARGET} --to slides --SlidesExporter.reveal_number='c/t' --SlidesExporter.reveal_scroll=True --output-dir {OUTPUT_FOLDER} --output {OUTPUT_NAME} & exit")
+    
+    original_output_path = os.path.join(OUTPUT_FOLDER, f"{OUTPUT_NAME}.{NBCONVERT_OUTPUT_SUFFIX}")
+    
+    output_extension = OUTPUT_NAME[OUTPUT_NAME.rfind('.'):]
+    output_extension = None if len(output_extension) == 1 else output_extension
+    new_output_path = os.path.join(OUTPUT_FOLDER, (OUTPUT_NAME + ".html") if not output_extension else OUTPUT_NAME)
+    if os.path.exists(new_output_path):
+        os.remove(new_output_path)
+        
+    os.rename(original_output_path, new_output_path)
